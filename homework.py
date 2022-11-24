e6c50a13eca8329e053432cd35b873e51b9514ab
Dealer86 <114797833+Dealer86@users.noreply.github.com>
2022-11-24 15:29:06 +0200

    Delete homework.py

--- homework.py
+++
@@ -1,8 +0,0 @@
-print("Hello homework")
-print("Vreau sa o tin codul asta")
-print("sau asta")
-print("o modificare linia 4")
-for i in range(10):
-  print("#",end="")
-print("asta sa fie pe github")
-print("Lo Ve")
